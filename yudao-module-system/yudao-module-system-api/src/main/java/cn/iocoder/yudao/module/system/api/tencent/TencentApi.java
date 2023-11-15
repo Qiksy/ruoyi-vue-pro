46a31b93fbd5444946ecd869264aad06f391b989
Qiksy <brianjohnsonp0i631@gmail.com>
2023-11-15 17:02:06 +0800

    feat 添加企业微信单点登录的功能

---
+++ yudao-module-system/yudao-module-system-api/src/main/java/cn/iocoder/yudao/module/system/api/tencent/TencentApi.java
@@ -0,0 +1,18 @@
+package cn.iocoder.yudao.module.system.api.tencent;
+
+import java.io.IOException;
+
+/**
+ * 这里是对接腾讯的接口
+ * @author linr
+ * @since 2023/11/15 15:29
+ */
+public interface TencentApi {
+
+    /**
+     * 获取access_token
+     */
+    String getAccessToken() throws IOException;
+
+    String resetAccessToken() throws IOException;
+}
---
+++ yudao-module-system/yudao-module-system-biz/src/main/java/cn/iocoder/yudao/module/system/api/tencent/TencentApiImpl.java
@@ -0,0 +1,72 @@
+package cn.iocoder.yudao.module.system.api.tencent;
+
+import cn.iocoder.yudao.module.system.config.TencentProperties;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@Slf4j
+public class TencentApiImpl implements TencentApi {
+
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    //从配置中读取corpid和corpsecret
+
+    @Resource
+    TencentProperties tencentProperties;
+
+    @Override
+    public String getAccessToken() throws IOException {
+        //从Redis中获取
+        String tencentAccessToken = stringRedisTemplate.opsForValue().get("TENCENT_ACCESS_TOKEN");
+        if (tencentAccessToken == null) {
+            //通过接口去获取新的access_token，然后设置到Redis中
+
+            //新建HTTP请求，调用腾讯接口
+            CloseableHttpClient client = HttpClients.createDefault();
+
+            HttpGet httpGet = new HttpGet("https://qyapi.weixin.qq.com/cgi-bin/gettoken?corpid=" + tencentProperties.getCorpId()
+                    + "&corpsecret=" + tencentProperties.getSecret());
+            CloseableHttpResponse response = client.execute(httpGet);
+            HttpEntity entity = response.getEntity();//获得实体内容
+            String content = EntityUtils.toString(entity, "utf-8");//通过实体工具类转换实体输出格式
+            System.out.println(content);
+            client.close();
+
+            ObjectMapper op = new ObjectMapper();
+            JsonNode jsonNode = op.readTree(content);
+            String accessToken = jsonNode.get("access_token").asText();
+            int expiresIn = jsonNode.get("expires_in").asInt();
+
+            //将获取到的access_token设置到Redis中
+            stringRedisTemplate.opsForValue().set("TENCENT_ACCESS_TOKEN",accessToken, expiresIn, TimeUnit.SECONDS);
+            return accessToken;
+        }
+        return tencentAccessToken;
+    }
+
+    @Override
+    public String resetAccessToken() throws IOException {
+        stringRedisTemplate.delete("TENCENT_ACCESS_TOKEN");
+
+        return getAccessToken();
+    }
+}
