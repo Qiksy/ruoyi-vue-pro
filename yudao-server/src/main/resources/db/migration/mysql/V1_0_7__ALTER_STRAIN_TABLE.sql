6fc30ad78eadc505b661bebfa6233343dbc2e38b
Qiksy <brianjohnsonp0i631@gmail.com>
2024-02-04 13:48:09 +0800

    fix: 修复字段

--- yudao-server/src/main/resources/db/migration/mysql/V1_0_7__ALTER_STRAIN_TABLE.sql
+++ yudao-server/src/main/resources/db/migration/mysql/V1_0_7__ALTER_STRAIN_TABLE.sql
@@ -2,7 +2,7 @@ ALTER TABLE `ruoyi-vue-pro`.`strain_freezing_tube_stock_info`
     ADD COLUMN `save_by_name` varchar(255) NULL COMMENT '保存人名称' AFTER `save_by`;
 
 ALTER TABLE `ruoyi-vue-pro`.`strain_freezing_tube_stock_info`
-    ADD COLUMN `stock_pre_entry_code` varchar(255) NULL COMMENT '预录入编号' AFTER `stock_pre_entry_id`
+    ADD COLUMN `stock_pre_entry_code` varchar(255) NULL COMMENT '预录入编号' AFTER `stock_pre_entry_id`;
 
 
 ALTER TABLE `ruoyi-vue-pro`.`strain_freezing_tube_stock_pre_entry`
