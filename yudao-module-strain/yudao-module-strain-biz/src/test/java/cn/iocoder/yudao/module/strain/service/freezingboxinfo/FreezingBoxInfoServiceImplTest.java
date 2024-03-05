5cf36c459f0902e26f9bddf0b399be4462a5be7d
Qiksy <brianjohnsonp0i631@gmail.com>
2024-03-05 16:56:42 +0800

    fix: 冻藏盒单元测试

--- yudao-module-strain/yudao-module-strain-biz/src/test/java/cn/iocoder/yudao/module/strain/service/freezingboxinfo/FreezingBoxInfoServiceImplTest.java
+++ yudao-module-strain/yudao-module-strain-biz/src/test/java/cn/iocoder/yudao/module/strain/service/freezingboxinfo/FreezingBoxInfoServiceImplTest.java
@@ -15,6 +15,7 @@ import cn.iocoder.yudao.framework.common.pojo.PageResult;
 
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Import;
+
 import java.util.*;
 import java.time.LocalDateTime;
 
@@ -30,6 +31,7 @@ import static org.mockito.Mockito.*;
 
 /**
  * {@link FreezingBoxInfoServiceImpl} 的单元测试类
+ * 冻藏盒 基础信息测试
  *
  * @author qiksy
  */
@@ -53,7 +55,7 @@ public class FreezingBoxInfoServiceImplTest extends BaseDbUnitTest {
         assertNotNull(freezingBoxInfoId);
         // 校验记录的属性是否正确
         FreezingBoxInfoDO freezingBoxInfo = freezingBoxInfoMapper.selectById(freezingBoxInfoId);
-        assertPojoEquals(reqVO, freezingBoxInfo);
+        assertPojoEquals(reqVO, freezingBoxInfo, "createTime", "updateTime");
     }
 
     @Test
@@ -92,8 +94,8 @@ public class FreezingBoxInfoServiceImplTest extends BaseDbUnitTest {
 
         // 调用
         freezingBoxInfoService.deleteFreezingBoxInfo(id);
-       // 校验数据不存在了
-       assertNull(freezingBoxInfoMapper.selectById(id));
+        // 校验数据不存在了
+        assertNull(freezingBoxInfoMapper.selectById(id));
     }
 
     @Test
@@ -106,62 +108,60 @@ public class FreezingBoxInfoServiceImplTest extends BaseDbUnitTest {
     }
 
     @Test
-    @Disabled  // TODO 请修改 null 为需要的值，然后删除 @Disabled 注解
     public void testGetFreezingBoxInfoPage() {
-       // mock 数据
-       FreezingBoxInfoDO dbFreezingBoxInfo = randomPojo(FreezingBoxInfoDO.class, o -> { // 等会查询到
-           o.setCode(null);
-           o.setName(null);
-           o.setRemark(null);
-       });
-       freezingBoxInfoMapper.insert(dbFreezingBoxInfo);
-       // 测试 code 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setCode(null)));
-       // 测试 name 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setName(null)));
-       // 测试 remark 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setRemark(null)));
-       // 准备参数
-       FreezingBoxInfoPageReqVO reqVO = new FreezingBoxInfoPageReqVO();
-       reqVO.setCode(null);
-       reqVO.setName(null);
-       reqVO.setRemark(null);
+        // mock 数据
+        FreezingBoxInfoDO dbFreezingBoxInfo = randomPojo(FreezingBoxInfoDO.class, o -> { // 等会查询到
+            o.setCode("001");
+            o.setName("基础冻藏盒");
+            o.setRemark("备注");
+        });
+        freezingBoxInfoMapper.insert(dbFreezingBoxInfo);
+        // 测试 code 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setCode("002")));
+        // 测试 name 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setName("测试冻藏盒")));
+        // 测试 remark 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setRemark("测试啊啊啊")));
+        // 准备参数
+        FreezingBoxInfoPageReqVO reqVO = new FreezingBoxInfoPageReqVO();
+        reqVO.setCode("001");
+        reqVO.setName("基础冻藏盒");
+        reqVO.setRemark("备注");
 
-       // 调用
-       PageResult<FreezingBoxInfoDO> pageResult = freezingBoxInfoService.getFreezingBoxInfoPage(reqVO);
-       // 断言
-       assertEquals(1, pageResult.getTotal());
-       assertEquals(1, pageResult.getList().size());
-       assertPojoEquals(dbFreezingBoxInfo, pageResult.getList().get(0));
+        // 调用
+        PageResult<FreezingBoxInfoDO> pageResult = freezingBoxInfoService.getFreezingBoxInfoPage(reqVO);
+        // 断言
+        assertEquals(1, pageResult.getTotal());
+        assertEquals(1, pageResult.getList().size());
+        assertPojoEquals(dbFreezingBoxInfo, pageResult.getList().getFirst());
     }
 
     @Test
-    @Disabled  // TODO 请修改 null 为需要的值，然后删除 @Disabled 注解
     public void testGetFreezingBoxInfoList() {
-       // mock 数据
-       FreezingBoxInfoDO dbFreezingBoxInfo = randomPojo(FreezingBoxInfoDO.class, o -> { // 等会查询到
-           o.setCode(null);
-           o.setName(null);
-           o.setRemark(null);
-       });
-       freezingBoxInfoMapper.insert(dbFreezingBoxInfo);
-       // 测试 code 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setCode(null)));
-       // 测试 name 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setName(null)));
-       // 测试 remark 不匹配
-       freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setRemark(null)));
-       // 准备参数
-       FreezingBoxInfoExportReqVO reqVO = new FreezingBoxInfoExportReqVO();
-       reqVO.setCode(null);
-       reqVO.setName(null);
-       reqVO.setRemark(null);
+        // mock 数据
+        FreezingBoxInfoDO dbFreezingBoxInfo = randomPojo(FreezingBoxInfoDO.class, o -> { // 等会查询到
+            o.setCode("001");
+            o.setName("基础冻藏盒");
+            o.setRemark("备注");
+        });
+        freezingBoxInfoMapper.insert(dbFreezingBoxInfo);
+        // 测试 code 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setCode("002")));
+        // 测试 name 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setName("测试冻藏盒")));
+        // 测试 remark 不匹配
+        freezingBoxInfoMapper.insert(cloneIgnoreId(dbFreezingBoxInfo, o -> o.setRemark("测试啊啊啊")));
+        // 准备参数
+        FreezingBoxInfoExportReqVO reqVO = new FreezingBoxInfoExportReqVO();
+        reqVO.setCode("001");
+        reqVO.setName("基础冻藏盒");
+        reqVO.setRemark("备注");
 
-       // 调用
-       List<FreezingBoxInfoDO> list = freezingBoxInfoService.getFreezingBoxInfoList(reqVO);
-       // 断言
-       assertEquals(1, list.size());
-       assertPojoEquals(dbFreezingBoxInfo, list.get(0));
+        // 调用
+        List<FreezingBoxInfoDO> list = freezingBoxInfoService.getFreezingBoxInfoList(reqVO);
+        // 断言
+        assertEquals(1, list.size());
+        assertPojoEquals(dbFreezingBoxInfo, list.getFirst());
     }
 
 }
